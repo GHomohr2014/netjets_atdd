1d32049367a13b3c94a3f1635f50b58eaf782bf4
GHomohr2014 <omohr@netjets.com>
2015-01-26 14:15:08 -0500

    Commit assessment from Monday

--- assessments/initial/ruby-assessment/test_05_strings.rb
+++ assessments/initial/ruby-assessment/test_05_strings.rb
@@ -4,17 +4,17 @@ class AboutStrings < Test::Unit::TestCase
 
   def test_is_a_double_quoted_string_a_string
     string = "Hello, World"
-    assert_equal __, string.is_a?(String)
+    assert_equal true, string.is_a?(String)
   end
 
   def test_is_a_single_quoted_strings_a_string
     string = 'Goodbye, World'
-    assert_equal __, string.is_a?(String)
+    assert_equal true, string.is_a?(String)
   end
 
   def test_string_concatenation
     string = "Hello, " + "World"
-    assert_equal __, string
+    assert_equal "Hello, World", string
   end
 
   def test_string_interpolation
